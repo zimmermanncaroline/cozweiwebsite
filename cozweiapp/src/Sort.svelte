6ed59c68f5feb4efc2055680e313f29073f9cd74
CZimmermann <line_zimmermann@web.de>
2023-09-25 21:05:15 +0200

    test_svelte

--- cozweiapp/src/Sort.svelte
+++ cozweiapp/src/Sort.svelte
@@ -1,11 +1,11 @@
 <script>
-  import {companies} from "./Companies.svelte";
-
+    import {companies} from "./Companies.svelte";
+    const listComp = [];
 </script>
 
 <div>
-    {#each companies as comp(comp.id)}
-    <p>{comp.compName}</p>
+    {#each companies as comp(compName)}
+    <p>{listComp.push()}</p>
     {/each}
 
 </div>
